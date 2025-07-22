3c9f6ddc04945fc9c62aecb496ef321e760ed295
Saksham <dawnimpulse@gmail.com>
2025-07-22 13:15:44 +0530

    feat(package): prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    semi: true,
+    tabWidth: 4,
+    printWidth: 80,
+    singleQuote: false,
+    tsxSingleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: "always",
+    endOfLine: "lf",
+    parser: "typescript",
+};
