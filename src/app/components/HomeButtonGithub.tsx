4da7914f5664f0cf40256b749191dd93ffe91f2b
Saksham <dawnimpulse@gmail.com>
2025-07-23 12:56:18 +0530

    feat(home): components - buttons - github & learn-more

---
+++ src/app/components/HomeButtonGithub.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { MButton } from "@/ui/button/MButton";
+import Image from "next/image";
+
+export function HomeButtonGithub() {
+    /**
+     * GitHub open tab
+     */
+    function githubOpen() {
+        window.open("https://github.com/codevry", "_blank");
+    }
+
+    return (
+        <MButton
+            click={githubOpen}
+            className="flex flex-row items-center gap-2 bg-black text-white"
+        >
+            <Image
+                src="/icons/github.svg"
+                alt="GitHub"
+                width={24}
+                height={24}
+            />
+            <span className="font-bold tracking-wide">GitHub</span>
+        </MButton>
+    );
+}
---
+++ src/app/components/HomeButtonLearnMore.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { MButton } from "@/ui/button/MButton";
+
+export function HomeButtonLearnMore() {
+    return (
+        <MButton className="ml-4 flex flex-row items-center gap-2 border-1 border-black">
+            <span className="font-bold tracking-wide">Learn More</span>
+        </MButton>
+    );
+}
