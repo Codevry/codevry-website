3c61481dcf375c86b42f157348415a03707cfae5
Saksham <dawnimpulse@gmail.com>
2025-07-25 14:17:49 +0530

    feat(home): tools - projects

--- src/app/(home)/components/ui/HomeTools.tsx
+++ src/app/(home)/components/ui/HomeTools.tsx
@@ -1,14 +1,20 @@
+import HomeToolsProjects from "@/app/(home)/components/ui/HomeToolsProjects";
+
 export default function HomeTools() {
     return (
         <div className="mt-24 flex w-screen justify-center px-24">
             <div className="flex w-full flex-col lg:w-8/10">
+                {/*heading*/}
                 <div className="w-full pr-4 text-end text-6xl font-semibold tracking-widest">
                     TOOLS
                 </div>
 
+                {/*horizontal line*/}
                 <div className="mt-4 h-[0.5px] w-full bg-neutral-300" />
 
-                <div className="h-64"></div>
+                {/*projects*/}
+                <div className="mt-24"></div>
+                <HomeToolsProjects />
             </div>
         </div>
     );
