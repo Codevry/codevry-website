40a146aa2a66b521d966de3345ae7bb8a6fee369
Saksham <dawnimpulse@gmail.com>
2025-07-25 12:24:47 +0530

    feat(home): ui - landing

---
+++ src/app/components/ui/HomeLanding.tsx
@@ -0,0 +1,46 @@
+import { DotBackground } from "@/ui/DotBackground";
+import { HomeButtonGithub } from "@/app/components/HomeButtonGithub";
+import { HomeButtonLearnMore } from "@/app/components/HomeButtonLearnMore";
+import HomeScroll from "@/app/components/ui/HomeScroll";
+
+export default function HomeLanding() {
+    return (
+        <div className="relative h-screen w-screen">
+            {/*bg beams*/}
+            <div className="absolute inset-0">
+                <DotBackground className="h-screen w-screen" />
+            </div>
+
+            {/*app name*/}
+            <div className="absolute z-20 mt-8 flex w-full flex-row justify-center">
+                <div className="font-bold tracking-widest text-black">
+                    CODEVRY
+                </div>
+            </div>
+
+            {/*title & button*/}
+            <div className="absolute inset-0 z-10 flex h-screen w-screen flex-col items-center justify-center">
+                {/*circle bg*/}
+                {/*<div className="relative self-end mr-16 size-[200px]  rounded-full mb-8 border-4 border-opacity-40 border-blue-500 bg-gradient-to-br from-blue-500 to-green-500 blur-[60px] opacity-60" />*/}
+
+                {/*title*/}
+                <div className="bg-gradient-to-b from-neutral-400 to-neutral-800 bg-clip-text text-center text-4xl leading-snug text-transparent">
+                    Building Open-Source <br />
+                    Tools, Services & Applications
+                </div>
+
+                {/*buttons*/}
+                <div className="mt-8 flex flex-row items-center">
+                    {/*github*/}
+                    <HomeButtonGithub />
+
+                    {/*learn more*/}
+                    <HomeButtonLearnMore />
+                </div>
+            </div>
+
+            {/*scroll*/}
+            <HomeScroll />
+        </div>
+    );
+}
