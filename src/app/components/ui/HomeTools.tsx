7a83b1a94d645d6b45e397cb0544c9159f322433
Saksham <dawnimpulse@gmail.com>
2025-07-25 12:24:36 +0530

    feat(home): ui - tools

---
+++ src/app/components/ui/HomeTools.tsx
@@ -0,0 +1,15 @@
+export default function HomeTools() {
+    return (
+        <div className="mt-24 flex w-screen justify-center px-24">
+            <div className="flex w-full flex-col xl:w-8/10">
+                <div className="text-6xl font-bold tracking-widest text-transparent">
+                    TOOLS
+                </div>
+
+                <div className="h-[0.5px] w-full bg-neutral-300" />
+
+                <div className="h-64"></div>
+            </div>
+        </div>
+    );
+}
