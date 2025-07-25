15a86c8be393fa8f7d2f14cef046c1514b3ccad2
Saksham <dawnimpulse@gmail.com>
2025-07-25 14:18:06 +0530

    feat(home): container - project - details & icons

--- src/app/(home)/components/container/HomeContainerProject.tsx
+++ src/app/(home)/components/container/HomeContainerProject.tsx
@@ -1,5 +1,6 @@
 import { TypeProject } from "@/types/TypeProject";
 import Image from "next/image";
+import { MIconButton } from "@/ui/button/MIconButton";
 
 export default function HomeContainerProject({
     project,
@@ -7,7 +8,7 @@ export default function HomeContainerProject({
     project: TypeProject;
 }) {
     return (
-        <div className="flex">
+        <div className="flex flex-row items-start">
             {/*icon*/}
             <Image
                 src={`/icons/${project.icon}`}
@@ -17,12 +18,35 @@ export default function HomeContainerProject({
             />
 
             {/*details*/}
-            <div className="flex-col">
+            <div className="ml-8 flex flex-col">
                 {/*title*/}
-                <div className="text-lg font-bold">{project.title}</div>
+                <div className="text-2xl font-bold">{project.title}</div>
 
                 {/*description*/}
-                <div className="mt-4 text-stone-500">{project.description}</div>
+                <div className="mt-2 text-lg text-zinc-400">
+                    {project.description}
+                </div>
+
+                {/*links & lang*/}
+                <div className="mt-4 flex flex-row">
+                    {/*links*/}
+
+                    {/*github*/}
+                    <MIconButton
+                        icon="/icons/github-zinc.svg"
+                        link={project.github}
+                        className="size-6 hover:scale-125"
+                    />
+
+                    {/*website*/}
+                    {project.website && (
+                        <MIconButton
+                            icon="/icons/web-zinc.svg"
+                            link={project.website}
+                            className="ml-4 size-6 hover:scale-125"
+                        />
+                    )}
+                </div>
             </div>
         </div>
     );
