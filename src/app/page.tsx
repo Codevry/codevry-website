62085d45ff10eeae3c77c575c1da4156bcdf0d03
Saksham <dawnimpulse@gmail.com>
2025-07-25 12:57:08 +0530

    feat(page): home - icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import HomeLanding from "@/app/components/ui/HomeLanding";
 import HomeTools from "@/app/components/ui/HomeTools";
+import HomeIcons from "@/app/components/ui/HomeIcons";
 
 export default function Page() {
     return (
         <div className="flex w-screen flex-col">
             <HomeLanding />
+            <HomeIcons />
             <HomeTools />
         </div>
     );
