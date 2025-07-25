8d9b35df81c06f6a344ae4164121f52b8a6712d4
Saksham <dawnimpulse@gmail.com>
2025-07-25 13:27:11 +0530

    feat(home): ui - tools changes in structure

--- src/app/components/ui/HomeTools.tsx
+++ src/app/components/ui/HomeTools.tsx
@@ -1,12 +1,12 @@
 export default function HomeTools() {
     return (
         <div className="mt-24 flex w-screen justify-center px-24">
-            <div className="flex w-full flex-col xl:w-8/10">
-                <div className="text-6xl font-bold tracking-widest text-transparent">
+            <div className="flex w-full flex-col lg:w-8/10">
+                <div className="w-full pr-4 text-end text-6xl font-semibold tracking-widest">
                     TOOLS
                 </div>
 
-                <div className="h-[0.5px] w-full bg-neutral-300" />
+                <div className="mt-4 h-[0.5px] w-full bg-neutral-300" />
 
                 <div className="h-64"></div>
             </div>
