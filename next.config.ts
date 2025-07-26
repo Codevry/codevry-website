a965626535ac30c4e9c1d1f54a36ddfc851f950d
Saksham <dawnimpulse@gmail.com>
2025-07-26 17:39:35 +0530

    feat(package): svgr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,39 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    turbopack: {
+        rules: {
+            // Define how Turbopack should handle .svg files
+            "*.svg": {
+                loaders: [
+                    {
+                        loader: "@svgr/webpack",
+                        options: {
+                            icon: true,
+                            // You can add SVGR options here. For example:
+                            // icon: true, // Treat SVG as an icon component
+                            // svgoConfig: {
+                            //   plugins: [
+                            //     {
+                            //       name: 'preset-default',
+                            //       params: {
+                            //         overrides: {
+                            //           removeViewBox: false,
+                            //         },
+                            //       },
+                            //     },
+                            //     'removeDimensions',
+                            //   ],
+                            // },
+                        },
+                    },
+                ],
+                // Important: Tell Turbopack to treat the output as a JavaScript module
+                as: "*.js",
+            },
+        },
+    },
 };
 
 export default nextConfig;
