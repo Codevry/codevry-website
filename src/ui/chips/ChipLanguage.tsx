c55ea1521631826210d0597587b3787b7eb04ba7
Saksham <dawnimpulse@gmail.com>
2025-07-26 18:10:35 +0530

    feat(ui): chips - language

---
+++ src/ui/chips/ChipLanguage.tsx
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+import { TypeProjectLang } from "@/types/TypeProject";
+
+export default function ChipLanguage({ lang }: { lang: TypeProjectLang }) {
+    return (
+        <div
+            className={cn(
+                "mx-2 flex flex-row items-center rounded-full px-8 py-1",
+                `${lang.bgColor}`,
+                `${lang.textColor}`,
+                "text-sm"
+            )}
+        >
+            {lang.name}
+        </div>
+    );
+}
