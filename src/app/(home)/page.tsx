2e5ecb48abf079e2e3b9de50e3ae1dcd1d676076
Saksham <dawnimpulse@gmail.com>
2025-08-20 17:36:58 +0530

    feat(home): page - services

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import HomeLanding from "@/app/(home)/components/ui/HomeLanding";
 import HomeTools from "@/app/(home)/components/ui/HomeTools";
 import HomeIcons from "@/app/(home)/components/ui/HomeIcons";
+import HomeServices from "@/app/(home)/components/ui/HomeServices";
 
 export default function Page() {
     return (
@@ -8,6 +9,7 @@ export default function Page() {
             <HomeLanding />
             <HomeIcons />
             <HomeTools />
+            <HomeServices />
         </div>
     );
 }
