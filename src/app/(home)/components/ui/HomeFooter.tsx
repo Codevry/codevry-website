ff5299f7bbd3526d9caec9471fc0704d7422bfeb
Saksham <dawnimpulse@gmail.com>
2025-08-21 15:51:20 +0530

    feat(home): footer - logo

--- src/app/(home)/components/ui/HomeFooter.tsx
+++ src/app/(home)/components/ui/HomeFooter.tsx
@@ -2,6 +2,8 @@
 
 import { twMerge } from "tailwind-merge";
 import HomeIconGithub from "@/app/(home)/components/HomeIconGithub";
+import { Logo } from "@/ui/icons/logo";
+import { ENUM_COLOR } from "@/app/utils/enums";
 
 interface HomeFooterProps {
     className?: string;
@@ -15,7 +17,7 @@ export default function HomeFooter({ className }: HomeFooterProps) {
                     "mx-4 mt-24 mb-8 flex w-full flex-col items-center justify-around gap-8 rounded-2xl bg-black py-6 text-white md:flex-row md:gap-0 lg:w-8/12"
                 }
             >
-                <div>Codevry</div>
+                <Logo color={ENUM_COLOR.WHITE} />
                 <div>© Codevry Labs, 2025</div>
                 <HomeIconGithub />
             </div>
