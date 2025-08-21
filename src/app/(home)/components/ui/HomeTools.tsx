3a066819a53bcdb0813690648138409e85e81d45
Saksham <dawnimpulse@gmail.com>
2025-08-21 12:13:46 +0530

    feat(home): tools - responsive

--- src/app/(home)/components/ui/HomeTools.tsx
+++ src/app/(home)/components/ui/HomeTools.tsx
@@ -2,10 +2,10 @@ import HomeToolsProjects from "@/app/(home)/components/ui/HomeToolsProjects";
 
 export default function HomeTools() {
     return (
-        <div className="mt-24 flex w-screen justify-center px-24">
+        <div className="mt-24 flex w-screen justify-center px-8 lg:px-24">
             <div className="flex w-full flex-col lg:w-8/10">
                 {/*heading*/}
-                <div className="font-montserrat w-fit rounded-2xl bg-gradient-to-r from-rose-400 to-transparent px-16 py-6 text-5xl tracking-widest">
+                <div className="font-montserrat w-fit rounded-2xl bg-gradient-to-r from-rose-400 to-transparent px-16 py-6 text-3xl tracking-widest md:text-5xl">
                     TOOLS
                 </div>
 
