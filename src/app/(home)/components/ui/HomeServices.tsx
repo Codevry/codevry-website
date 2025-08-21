73554c488f5c8243bdd447151bef8f4792cd270f
Saksham <dawnimpulse@gmail.com>
2025-08-21 12:13:41 +0530

    feat(home): services - responsive

--- src/app/(home)/components/ui/HomeServices.tsx
+++ src/app/(home)/components/ui/HomeServices.tsx
@@ -2,10 +2,10 @@ import HomeServicesProjects from "@/app/(home)/components/ui/HomeServicesProject
 
 export default function HomeServices() {
     return (
-        <div className="mt-36 flex w-screen justify-center px-24">
+        <div className="mt-36 flex w-screen justify-center px-8 lg:px-24">
             <div className="flex w-full flex-col lg:w-8/10">
                 {/*heading*/}
-                <div className="font-montserrat w-fit rounded-2xl bg-gradient-to-r from-green-400 to-transparent px-16 py-6 text-5xl tracking-widest">
+                <div className="font-montserrat w-fit rounded-2xl bg-gradient-to-r from-green-400 to-transparent px-16 py-6 text-3xl tracking-widest md:text-5xl">
                     SERVICES
                 </div>
 
