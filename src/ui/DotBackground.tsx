494de285c0dd1ab3d21c1fa6ecb10775e84c5421
Saksham <dawnimpulse@gmail.com>
2025-08-30 11:38:55 +0530

    fix: dark mode

--- src/ui/DotBackground.tsx
+++ src/ui/DotBackground.tsx
@@ -5,7 +5,7 @@ export function DotBackground({ className }: { className?: string }) {
     return (
         <div
             className={cn(
-                "relative flex h-[50rem] w-full items-center justify-center bg-white dark:bg-black",
+                "relative flex h-[50rem] w-full items-center justify-center bg-white",
                 className
             )}
         >
@@ -13,12 +13,11 @@ export function DotBackground({ className }: { className?: string }) {
                 className={cn(
                     "absolute inset-0",
                     "[background-size:20px_20px]",
-                    "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-                    "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"
+                    "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]"
                 )}
             />
             {/* Radial gradient for the container to give a faded look */}
-            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         </div>
     );
 }
